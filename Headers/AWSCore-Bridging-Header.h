0ffb6c258e5497e8d617f0325ce64304282a3aa9
Jonathand Alberto Serrano Serrano <jonathandserrano@rccl.com>
2021-02-17 11:33:25 -0500

    Adding new AWSS3 Modules

---
+++ Headers/AWSCore-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  AWSCore-Bridging-Header.h
+//  
+//
+//  Created by Jonathand Alberto Serrano Serrano on 17/02/21.
+//
+
+#import <iOS/AWSCore.h>
---
+++ Headers/AWSS3-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  AWSS3-Bridging-Header.h
+//  
+//
+//  Created by Jonathand Alberto Serrano Serrano on 17/02/21.
+//
+
+#import <iOS/AWSS3.h>
